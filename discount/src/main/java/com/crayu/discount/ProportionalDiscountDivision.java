3cb0f62dc7fe6e1ccb21df8cc85854c4c79d4486
CR4YU <craayu@gmail.com>
2019-06-20 11:01:37 +0200

    ProportionalDiscountDivision strategy implementation

---
+++ discount/src/main/java/com/crayu/discount/ProportionalDiscountDivision.java
@@ -0,0 +1,47 @@
+package com.crayu.discount;
+
+import com.crayu.model.Product;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+
+public class ProportionalDiscountDivision implements DiscountDivision {
+
+    /**
+     * This implementation splits the discount proportionally.
+     * In case cannot split the remaining discount is added to last product.
+     * In case discount >= products.prices.sum return products.prices
+     *
+     * @param products products
+     * @param discount discount
+     * @return discount per product
+     */
+    @Override
+    public BigDecimal[] discountPerProduct(List<Product> products, BigDecimal discount) {
+        if (discount.doubleValue() < 0)
+            throw new IllegalArgumentException("Discount cannot be lower than 0");
+
+        BigDecimal sum = products.stream()
+                .map(Product::getPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        if (discount.doubleValue() >= sum.doubleValue()) {
+            return products.stream()
+                    .map(Product::getPrice)
+                    .toArray(BigDecimal[]::new);
+        }
+        BigDecimal[] discountPerProduct = new BigDecimal[products.size()];
+        BigDecimal discountSum = BigDecimal.ZERO;
+
+        for (int i = 0; i < products.size(); i++) {
+            BigDecimal price = products.get(i).getPrice();
+            discountPerProduct[i] =  price.divide(sum,2, RoundingMode.DOWN).multiply(discount);
+            discountSum = discountSum.add(discountPerProduct[i]);
+        }
+
+        discountPerProduct[discountPerProduct.length - 1] =
+                discountPerProduct[discountPerProduct.length - 1].add(discount.subtract(discountSum));
+
+        return discountPerProduct;
+    }
+}
